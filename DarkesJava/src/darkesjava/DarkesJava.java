37b41a4f2d29ad99a63202958d4a5e065fc3a71d
JustinDuban <justin.duban@free.fr>
2018-10-25 11:44:56 +0200

    r

--- DarkesJava/src/darkesjava/DarkesJava.java
+++ DarkesJava/src/darkesjava/DarkesJava.java
@@ -16,6 +16,14 @@ public class DarkesJava {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+       Aventurier nain = new Aventurier("Gurdil", 11, 70, 40, 20);
+       Aventurier elfe = new Aventurier("Tirael", 8, 40, 70, 50);
+       Aventurier mage = new Aventurier("Bandoulf", 7, 20, 30, 70);
+       
+       Ennemi gobl1 = new Ennemi("Zibli", 5, "2", "Garboulag!");
+       Ennemi gobl2 = new Ennemi("Billi", 5, "2", "Garboulug!");
+       Ennemi gobl3 = new Ennemi("Guili", 5, "2", "Garboulog!");
     }
     
 }
